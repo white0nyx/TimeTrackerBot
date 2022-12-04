e4debea3bb32840b91f38581f67caa3627eebd3c
Anatoly Kushnarenko <Anatoly.kushnarenko@gmail.com>
2022-12-04 21:02:06 +0300

    Добавлены кнопки отмены в разделы по изменению категории

--- tgbot/handlers/cancel.py
+++ tgbot/handlers/cancel.py
@@ -9,7 +9,7 @@ from tgbot.misc.states import States
 
 async def cancel_button(message: Message, state: FSMContext):
     """Обработка нажатия на кнопку отмены"""
-    await state.reset_state(with_data=False)
+    await state.reset_state(with_data=True)
     async with state.proxy() as data:
         data['suspect_category'] = {}
     await message.answer('Вы вернулись в главное меню', reply_markup=main_keyboard)
@@ -18,4 +18,8 @@ async def cancel_button(message: Message, state: FSMContext):
 def register_cancel_button(dp: Dispatcher):
     """Регистрация обработчика кнопки отмена"""
     dp.register_message_handler(cancel_button, Text('↩ Отмена'), state=[States.add_new_category_name,
-                                                                        States.add_new_category_based_minutes])
+                                                                        States.add_new_category_based_minutes,
+                                                                        States.wait_add_minutes,
+                                                                        States.wait_sub_minutes,
+                                                                        States.wait_new_title,
+                                                                        States.confirm_delete_category])
--- tgbot/handlers/category_edit.py
+++ tgbot/handlers/category_edit.py
@@ -4,7 +4,9 @@ from aiogram import Dispatcher
 from aiogram.dispatcher import FSMContext
 from aiogram.types import CallbackQuery, Message
 
+from tgbot.handlers.cancel import cancel_button
 from tgbot.keyboards.inline import category_buttons, yes_no_keyboard
+from tgbot.keyboards.reply import cancel_button, main_keyboard
 from tgbot.misc.states import States
 from tgbot.misc.work_with_date import get_day_of_week
 from tgbot.misc.work_with_json import get_user_from_json_db, update_user_data, fill_all_categories_past_date
@@ -42,16 +44,19 @@ async def edit_category(call: CallbackQuery, state: FSMContext):
 
     if call.data == 'add_time':
         await call.message.answer(
-            f'Введите время в минутах, которое вы хотите добавить к категории {category_title}:')
+            f'Введите время в минутах, которое вы хотите добавить к категории {category_title}:',
+            reply_markup=cancel_button)
         await States.wait_add_minutes.set()
 
     elif call.data == 'subtract_time':
         await call.message.answer(
-            f'Введите время в минутах, которое вы хотите вычесть из категории {category_title}:')
+            f'Введите время в минутах, которое вы хотите вычесть из категории {category_title}:',
+            reply_markup=cancel_button)
         await States.wait_sub_minutes.set()
 
     elif call.data == 'change_title':
-        await call.message.answer(f'Введите новое название для категории {category_title}:')
+        await call.message.answer(f'Введите новое название для категории {category_title}:',
+                                  reply_markup=cancel_button)
         await States.wait_new_title.set()
 
     elif call.data == 'delete_category':
@@ -102,7 +107,7 @@ async def change_time(call: CallbackQuery, state: FSMContext):
             minutes_text = get_word_end_rp(time_in_minutes)
         await call.message.delete()
         await call.message.answer(f'Вы отменили {action} {time_in_minutes} {minutes_text}! '
-                                  f'Введите новое число или нажмите отмену.')
+                                  f'Введите новое число или нажмите отмену.', reply_markup=cancel_button)
         await state_again.set()
 
     else:
@@ -139,7 +144,7 @@ async def change_time(call: CallbackQuery, state: FSMContext):
         update_user_data(user_id, user)
 
         await call.message.delete()
-        await call.message.answer(f'Время {action}!')
+        await call.message.answer(f'✅ Время {action}!', reply_markup=main_keyboard)
         await state.reset_state(with_data=True)
 
 
@@ -168,7 +173,7 @@ async def confirm_new_category_title(call: CallbackQuery, state: FSMContext):
 
     if call.data == 'no':
         await call.message.delete()
-        await call.message.answer('Введите другое название или воспользуйтесь отменой')
+        await call.message.answer('Введите другое название или воспользуйтесь отменой', reply_markup=cancel_button)
         await States.wait_new_title.set()
 
     else:
@@ -188,7 +193,7 @@ async def confirm_new_category_title(call: CallbackQuery, state: FSMContext):
 
         update_user_data(user_id, user)
 
-        await call.message.answer('Название категории успешно изменено!')
+        await call.message.answer('✅ Название категории успешно изменено!', reply_markup=main_keyboard)
         await state.reset_state(with_data=True)
 
 
@@ -200,7 +205,7 @@ async def confirm_delete_category(call: CallbackQuery, state: FSMContext):
     await call.answer(cache_time=10)
 
     if call.data == 'no':
-        await call.message.answer('Удаление категории отменено')
+        await call.message.answer('Удаление категории отменено', reply_markup=main_keyboard)
         await call.message.delete()
         await state.reset_state(with_data=True)
 
@@ -219,7 +224,7 @@ async def confirm_delete_category(call: CallbackQuery, state: FSMContext):
 
         update_user_data(user_id, user)
 
-        await call.message.answer(f'Категория {category_name} была удалена!')
+        await call.message.answer(f'✅ Категория {category_name} была удалена!', reply_markup=main_keyboard)
         await state.reset_state(with_data=True)
 
 
